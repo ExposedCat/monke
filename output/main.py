d5554f4083f763a36230a6572edefbdf85c9b775
Artem Prokop <artem13.prokop@gmail.com>
2024-08-02 12:45:19 +0200

    feat: TTS (output) model

---
+++ output/main.py
@@ -0,0 +1,16 @@
+import torch
+import os
+from TTS.api import TTS
+from playsound import playsound
+
+if not torch.cuda.is_available():
+  print('Failed to start: ROCm torch not found')
+  exit(1)
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to("cuda")
+
+def play_text(text: str):
+  output = "/tmp/talking-monkey-output.mp3"
+  tts.tts_to_file(text=text, speaker_wav="sample.mp3", language="en", file_path=output)
+  playsound(output)
+  os.remove(output)
